2fffd8637144f3d7c684991112e7394985212e4d
Suritam-Nandy <13suritsuritam@gmail.com>
2021-09-06 13:24:23 +0530

    🐛

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -1,26 +1,30 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 export default class ContactUs extends Component {
   render() {
     let resumeData = this.props.resumeData;
     return (
       <section id="contact">
-          <div className="row section-head">
-            <div className="ten columns">
-              <p className="lead">
+        <div className="row section-head">
+          <div className="ten columns">
+            <p className="lead">
               Feel free to contact me for any work or suggestions below
-              </p>
+            </p>
+          </div>
+        </div>
+        <div className="row">
+          <aside className="eigth columns footer-widgets">
+            <div className="widget">
+              <a
+                href="https://www.linkedin.com/in/suritam-nandy-74952718b/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <h4>Linked in : {resumeData.linkedinId}</h4>
+              </a>
             </div>
-          </div>
-          <div className="row">
-            <aside className="eigth columns footer-widgets">
-              <div className="widget">
-                <h4>Linked in :
-                  {resumeData.linkedinId}
-                </h4>
-              </div>
-            </aside>
-          </div>
-        </section>
-        );
+          </aside>
+        </div>
+      </section>
+    );
   }
 }
--- src/data-config.js
+++ src/data-config.js
@@ -1,126 +1,127 @@
 let resumeData = {
-  
-    "name": "Suritam Nandy",
-    "role": "Frontend Developer and a keen ML enthusiast",
-    "linkedinId":"Your LinkedIn Id",
-    "skypeid": "Your skypeid",
-    "roleDescription": " I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free hour.",
-    "socialLinks":[
-        {
-          "name":"linkedin",
-          "url":"https://www.linkedin.com/in/suritam-nandy-74952718b/",
-          "className":"fa fa-linkedin"
-        },
-        {
-          "name":"github",
-          "url":"https://github.com/Suritam-Nandy",
-          "className":"fa fa-github"
-        },
-        {
-          "name":"twitter",
-          "url":"https://twitter.com/NandySuritam",
-          "className":"fa fa-twitter"
-        }
-      ],
-    "aboutme":"I am currently a 2nd year student at SRM Institute of Science and Technology and pursuing my B.Tech Computer Science and Engineering. I am a self taught Full Stack Web Developer, currently diving deeper into Machine Learning. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
-    "address":"India",
-    "website":"https://github.com/Suritam-Nandy",
-    "education":[
-      {
-        "UniversityName":"SRM Institute of Science and Technology",
-        "specialization":"B.Tech (Computer Science and Engineering)",
-        "MonthOfPassing":"",
-        "YearOfPassing":"2023",
-        "Achievements":""
-      },
-      {
-        "UniversityName":"DAV Model School, Durgapur",
-        "specialization":"CBSE",
-        "MonthOfPassing":"",
-        "YearOfPassing":"2019",
-        "Achievements":"Percentage: 87.8%"
-      },
-      {
-        "UniversityName":"St. Xavier's School, Durgapur",
-        "specialization":"ICSE",
-        "MonthOfPassing":"",
-        "YearOfPassing":"2017",
-        "Achievements":"Percentage: 88.3%"
-      }
-    ],
-    "work":[
-      {
-        "CompanyName":"SRMKZILLA || Mozilla Campus Club || SRM-IST",
-        "specialization":"TECHNICAL MEMBER",
-        "MonthOfLeaving":"",
-        "YearOfLeaving":"PRESENT",
-        "Achievements":"Building in House Projects :>"
-      },
-      {
-        "CompanyName":"SRMKZILLA || Mozilla Campus Club || SRM-IST",
-        "specialization":"EVENTS MEMBER",
-        "MonthOfLeaving":"June",
-        "YearOfLeaving":"2020",
-        "Achievements":"Drafted some Iconic Events :x"
-      }
-    ],
-    "skillsDescription":"",
-    "skills":[
-      {
-        "skillname":"HTML5"
-      },
-      {
-        "skillname":"CSS"
-      },
-      {
-        "skillname":"ReactJs"
-      },
-      {
-        "skillname":"Illustrator"
-      },
-      {
-        "skillname":"Python"
-      },
-      {
-        "skillname":"openCV"
-      }
-    ],
-    "portfolio":[
-      {
-        "name":"EatFit",
-        "description":"web-app",
-        "imgurl":"images/portfolio/coffee.jpg"
-      },
-      {
-        "name":"CEFORO Vision",
-        "description":"Voice Assistant",
-        "imgurl": "images/portfolio/coffee.jpg",
-        "repolink": "",
-        "link":"https://ceforo.tech/",
-        
-      },
-      {
-        "name":"project3",
-        "description":"mobileapp",  
-        "imgurl":"images/portfolio/coffee.jpg"
-      },
-      {
-        "name":"project4",
-        "description":"mobileapp",
-        "imgurl":"images/portfolio/coffee.jpg"
-      }
-    ],
-    "testimonials":[
-      {
-        "description":"Life is like riding a bicycle. To keep your balance, you must keep moving.",
-        "name":"Albert Einstein"
-      },
-      {
-        "description":"Live as if you were to die tomorrow. Learn as if you were to live forever.",
-        "name":"Mahatma Gandhi"
-      }
-      
-    ]
-  }
-  
-  export default resumeData
+  name: "Suritam Nandy",
+  role: "Frontend Developer and a keen ML enthusiast",
+  linkedinId: "Suritam-Nandy",
+  skypeid: "Your skypeid",
+  roleDescription:
+    " I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free hour.",
+  socialLinks: [
+    {
+      name: "linkedin",
+      url: "https://www.linkedin.com/in/suritam-nandy-74952718b/",
+      className: "fa fa-linkedin",
+    },
+    {
+      name: "github",
+      url: "https://github.com/Suritam-Nandy",
+      className: "fa fa-github",
+    },
+    {
+      name: "twitter",
+      url: "https://twitter.com/NandySuritam",
+      className: "fa fa-twitter",
+    },
+  ],
+  aboutme:
+    "I am currently a 2nd year student at SRM Institute of Science and Technology and pursuing my B.Tech Computer Science and Engineering. I am a self taught Full Stack Web Developer, currently diving deeper into Machine Learning. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
+  address: "India",
+  website: "https://github.com/Suritam-Nandy",
+  education: [
+    {
+      UniversityName: "SRM Institute of Science and Technology",
+      specialization: "B.Tech (Computer Science and Engineering)",
+      MonthOfPassing: "",
+      YearOfPassing: "2023",
+      Achievements: "",
+    },
+    {
+      UniversityName: "DAV Model School, Durgapur",
+      specialization: "CBSE",
+      MonthOfPassing: "",
+      YearOfPassing: "2019",
+      Achievements: "Percentage: 87.8%",
+    },
+    {
+      UniversityName: "St. Xavier's School, Durgapur",
+      specialization: "ICSE",
+      MonthOfPassing: "",
+      YearOfPassing: "2017",
+      Achievements: "Percentage: 88.3%",
+    },
+  ],
+  work: [
+    {
+      CompanyName: "SRMKZILLA || Mozilla Campus Club || SRM-IST",
+      specialization: "TECHNICAL MEMBER",
+      MonthOfLeaving: "",
+      YearOfLeaving: "PRESENT",
+      Achievements: "Building in House Projects :>",
+    },
+    {
+      CompanyName: "SRMKZILLA || Mozilla Campus Club || SRM-IST",
+      specialization: "EVENTS MEMBER",
+      MonthOfLeaving: "June",
+      YearOfLeaving: "2020",
+      Achievements: "Drafted some Iconic Events :x",
+    },
+  ],
+  skillsDescription: "",
+  skills: [
+    {
+      skillname: "HTML5",
+    },
+    {
+      skillname: "CSS",
+    },
+    {
+      skillname: "ReactJs",
+    },
+    {
+      skillname: "Illustrator",
+    },
+    {
+      skillname: "Python",
+    },
+    {
+      skillname: "openCV",
+    },
+  ],
+  portfolio: [
+    {
+      name: "EatFit",
+      description: "web-app",
+      imgurl: "images/portfolio/coffee.jpg",
+    },
+    {
+      name: "CEFORO Vision",
+      description: "Voice Assistant",
+      imgurl: "images/portfolio/coffee.jpg",
+      repolink: "",
+      link: "https://ceforo.tech/",
+    },
+    {
+      name: "project3",
+      description: "mobileapp",
+      imgurl: "images/portfolio/coffee.jpg",
+    },
+    {
+      name: "project4",
+      description: "mobileapp",
+      imgurl: "images/portfolio/coffee.jpg",
+    },
+  ],
+  testimonials: [
+    {
+      description:
+        "Life is like riding a bicycle. To keep your balance, you must keep moving.",
+      name: "Albert Einstein",
+    },
+    {
+      description:
+        "Live as if you were to die tomorrow. Learn as if you were to live forever.",
+      name: "Mahatma Gandhi",
+    },
+  ],
+};
+
+export default resumeData;
